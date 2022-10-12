bc9ea30863f46ca0138ceca353f5ae60951ff470
sergey <33asasergey33@gmail.com>
2022-10-12 18:51:47 +0300

    fix: typography

--- src/shared/components/typography/typography.tsx
+++ src/shared/components/typography/typography.tsx
@@ -7,17 +7,18 @@ export interface TypographyProps {
    * The content
    */
   children: React.ReactNode;
-
   component: React.ElementType;
+  variant: 'h1' | 'title' | 'result';
 }
 
 export const Typography = (props: TypographyProps): JSX.Element => {
   const {
     children,
     component: Component = 'span',
+    variant,
   } = props;
 
   return (
-    <Component>{children}</Component>
+    <Component className={styles[variant]}>{children}</Component>
   );
 };
