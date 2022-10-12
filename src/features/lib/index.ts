6f8d8e66eab4ce99a876671e948c6a4598fc8f4e
sergey <33asasergey33@gmail.com>
2022-10-12 18:54:00 +0300

    feat: add math function

---
+++ src/features/lib/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+export const getInitialPayment = (firstPaymentPercent: number, carCost: number): number => Math.trunc((firstPaymentPercent * carCost) / 100);
+
+export const getMonthlyPayment = (initialPayment: number, carCoast: number, months: number): number => {
+  const payment = (carCoast - initialPayment) * ((0.035 * ((1 + 0.035) ** months)) / (((1 + 0.035) ** months) - 1));
+
+  return Math.trunc(payment);
+};
+
+export const getFullSum = (firstPaymentPercent: number, carCost: number, months: number): number => {
+  const initialPayment = getInitialPayment(firstPaymentPercent, carCost);
+
+  const mothPayment = getMonthlyPayment(initialPayment, carCost, months);
+
+  const fullSum = initialPayment + mothPayment * months;
+
+  return fullSum;
+};
+
+export const limitNumberWithinRange = (num: number, min = -Infinity, max = Infinity) => Math.min(Math.max(num, min), max);
+
+export const isNumeric = (str: string) => {
+  if (typeof str !== 'string') return false;
+
+  return !Number.isNaN(+str) && !Number.isNaN(parseFloat(str));
+};
