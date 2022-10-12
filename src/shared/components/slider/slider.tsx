db740be42161743e9ba97df0c1fc9e921b927840
sergey <33asasergey33@gmail.com>
2022-10-12 18:52:10 +0300

    feat: add slider

---
+++ src/shared/components/slider/slider.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import * as SliderPrimitive from '@radix-ui/react-slider';
+
+import { limitNumberWithinRange } from '../../../features/lib';
+
+import styles from './slider.module.scss';
+
+export interface SliderProps {
+  minValue: number;
+  maxValue: number;
+  onValueChange: (value: number[]) => void;
+  value: number;
+}
+
+export const Slider = (props: SliderProps): JSX.Element => {
+  const {
+    minValue,
+    maxValue,
+    onValueChange,
+    value,
+  } = props;
+
+  return (
+    <SliderPrimitive.Root
+      className={styles.root}
+      min={minValue}
+      max={maxValue}
+      step={1}
+      onValueChange={onValueChange}
+      value={[limitNumberWithinRange(value, minValue, maxValue)]}
+    >
+      <SliderPrimitive.Track className={styles.track}>
+        <SliderPrimitive.Range className={styles.range} />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className={styles.thumb} />
+    </SliderPrimitive.Root>
+  );
+};
